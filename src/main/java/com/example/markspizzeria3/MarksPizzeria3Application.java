4a5d086baed5642e1f1acabc5a5ecd2e11373932
Mark Popelier <mpopelier@nexient.com>
2023-11-03 15:40:33 -0400

    Initial commit

---
+++ src/main/java/com/example/markspizzeria3/MarksPizzeria3Application.java
@@ -0,0 +1,13 @@
+package com.example.markspizzeria3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarksPizzeria3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarksPizzeria3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/markspizzeria3/MarksPizzeria3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markspizzeria3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarksPizzeria3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
